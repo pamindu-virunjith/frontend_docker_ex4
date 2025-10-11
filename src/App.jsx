bc63962fbf23645f72b2f1c004065238f9b891f8
Pamindu Virunjith <paminduvirunjith2002@gmail.com>
2025-10-11 23:43:47 +0530

    first vite-react frontend project dockerizing.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="">
+        <h1>
+          This is My first docker Containerized vite-react Project
+        </h1>
+        <p>Pamindu VIrunjith De silva</p>
+      </div>
+    </>
+  )
+}
+
+export default App
