7451f56dccefc3c057709c3e9efc9b44a2bbf22e
Pamindu Virunjith <paminduvirunjith2002@gmail.com>
2025-10-12 22:34:50 +0530

    changes are bounded with volumes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <div className="">
         <h1>
-          This is My first docker Containerized vite-react Project
+          This is My first docker Containerized vite-react Project, with Docker
         </h1>
         <h2>Volumes is bounded Now!!</h2>
         <p>W.A Pamindu VIrunjith De silva.</p>
